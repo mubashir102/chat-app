86a7eddf0a8126d355a3ca8b18e83210d23cbd47
techMubashir <mubashir@4xportal.com>
2023-09-26 12:06:32 +0500

    first commit

---
+++ routes/userRouters.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const userController = require("./../controllers/userController");
+const router = express.Router();
+
+router.post("/signup", userController.signup);
+router.post("/signin", userController.signin);
+router.get("/logout", userController.logout);
+router.post("/forgotPassword", userController.forgotPassword);
+router.patch("/resetPassword/:token", userController.resetPassword);
+router.get("/login", userController.login);
+router.get("/register", userController.register);
+router.get("/forgot_password", userController.forgot_password);
+router.get("/reset_password", userController.reset_password);
+router.get("/", userController.index);
+
+router.get("/articles", userController.articles);
+router.get("/faq", userController.faq);
+router.get("/description", userController.description);
+// router.get("/detail_article", userController.detail_article);
+
+module.exports = router;
